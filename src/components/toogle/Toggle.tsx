3839cd6045604e40891b941a0d6b8e9e83df732c
Frederick Amoako-Atta <amoakofredatta@gmail.com>
2025-01-20 02:59:46 +0000

    Setting up configuration for github pages deployment

--- src/components/toogle/Toggle.tsx
+++ src/components/toogle/Toggle.tsx
@@ -3,15 +3,20 @@ import {useState} from "react";
 
 interface ToggleProps {
     label? : string,
-    hasLabel ? : boolean,
+    hasLabel ? : boolean
 }
 
 const Toggle = ({label, hasLabel}: ToggleProps) => {
     const [defaultVal, setDefaultVal] = useState(false);
+
+    const toggleHandler = () => {
+        setDefaultVal(!defaultVal);
+    }
+
     return (
         <>
             <div className="toggle-container">
-                <div aria-label={'switch'} className={defaultVal ? 'toggle active' : 'toggle'} onClick={() => setDefaultVal(!defaultVal)}></div>
+                <div aria-label={'switch'} className={defaultVal ? 'toggle active' : 'toggle'} onClick={toggleHandler}></div>
                 {hasLabel && <span className="label" aria-label={label}>{label}</span>}
             </div>
         </>
